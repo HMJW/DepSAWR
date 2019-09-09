570cce3d23fb12310f2fe03cd329b2130a5d2393
HMJW <767257113@qq.com>
2019-09-09 22:53:09 +0800

    modify baseline config

--- machine_translation/ZNMTBase/run.sh
+++ machine_translation/ZNMTBase/run.sh
@@ -1,1 +1,1 @@
-nohup python3 -u driver/Train.py --config_file default.cfg --thread 1 > train.log 2>&1 &
+nohup python -u driver/Train.py --config_file ./en-de-model/config.cfg --thread 1 --gpu 0 > train.log 2>&1 &
