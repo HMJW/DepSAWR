3a9c6534fd696f29b8407d69e84b6dce849a81ce
HMJW <767257113@qq.com>
2019-09-10 14:21:34 +0800

    modify default config

--- machine_translation/ZNMTJSYN/run.sh
+++ machine_translation/ZNMTJSYN/run.sh
@@ -1,1 +1,1 @@
-nohup python3 -u driver/Train.py --config_file default.cfg --parser_config_file parser.cfg --thread 1 > train.log 2>&1 &
+nohup python -u driver/Train.py --config_file default.cfg --parser_config_file parser.cfg --thread 1 --gpu 1 > train.log 2>&1 &
