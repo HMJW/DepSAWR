f477d86478d75734dd762be4b391108fa63cc7fa
HMJW <767257113@qq.com>
2019-09-11 18:16:52 +0800

    modify default config

--- machine_translation/ZNMTBase/run.sh
+++ machine_translation/ZNMTBase/run.sh
@@ -1,1 +1,1 @@
-nohup python -u driver/Train.py --config_file ./en-de-model/config.cfg --thread 1 --gpu 0 > train.log 2>&1 &
+python -u driver/Train.py --config_file ./en-de-model/config.cfg --thread 1 --gpu 0
