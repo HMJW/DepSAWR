3cfa9601e0aa95acd62de332860faee3851c3a2b
HMJW <767257113@qq.com>
2019-09-15 11:09:54 +0800

    default config

--- machine_translation/ZNMTJSYN/run.sh
+++ machine_translation/ZNMTJSYN/run.sh
@@ -1,1 +1,1 @@
-nohup python -u driver/Train.py --config_file default.cfg --parser_config_file parser.cfg --thread 1 --gpu 1 > train.log 2>&1 &
+python -u driver/Train.py --config_file default.cfg --parser_config_file parser.cfg --thread 1 --gpu 3
