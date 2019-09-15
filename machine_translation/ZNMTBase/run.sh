d859136cd81dd745a80bf8f0f280275d722edb27
HMJW <767257113@qq.com>
2019-09-15 10:51:15 +0800

    best config

--- machine_translation/ZNMTBase/run.sh
+++ machine_translation/ZNMTBase/run.sh
@@ -1,1 +1,1 @@
-python -u driver/Train.py --config_file ./en-de-model/config.cfg --thread 1 --gpu 4
+python -u driver/Train.py --config_file ./en-de-model/config.cfg --thread 1 --gpu 5
--- machine_translation/ZNMTBase/test.sh
+++ machine_translation/ZNMTBase/test.sh
@@ -1,7 +1,7 @@
-config_file=/data/wjiang/nmt-exp/en-de-model3/config.cfg
-best_step=168096
+config_file=/data/wjiang/nmt-exp/en-de-model2/config.cfg
+best_step=294168
 src_file=/data/wjiang/data/NMT/ch-en-mt/nist06.cn
 tgt_file=/data/wjiang/data/NMT/ch-en-mt/nist06.en
-gpu=5
+gpu=2
 python -u driver/test.py --config_file=$config_file --best_step=$best_step --src_file=$src_file --tgt_file=$tgt_file --gpu=$gpu
 
