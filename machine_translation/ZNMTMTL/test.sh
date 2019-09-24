afe1a71173e0e035565773b63b99f6c66db19820
HMJW <767257113@qq.com>
2019-09-24 12:21:39 +0800

    test bug

--- machine_translation/ZNMTMTL/test.sh
+++ machine_translation/ZNMTMTL/test.sh
@@ -1,7 +1,7 @@
-config_file=/data/wjiang/nmt-exp/en-de-model2/config.cfg
-best_step=294168
-src_file=/data/wjiang/data/NMT/ch-en-mt/nist06.cn
-tgt_file=/data/wjiang/data/NMT/ch-en-mt/nist06.en
-gpu=2
+config_file=/data2/wjiang/nmt-exp/mtl/config.cfg
+best_step=357204
+src_file=/data2/wjiang/data/NMT/ch-en-mt/nist06.cn
+tgt_file=/data2/wjiang/data/NMT/ch-en-mt/nist06.en
+gpu=5
 python -u driver/test.py --config_file=$config_file --best_step=$best_step --src_file=$src_file --tgt_file=$tgt_file --gpu=$gpu
 
